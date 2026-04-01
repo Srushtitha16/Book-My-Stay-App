7dc913745ac0b1748df8fa13b471885a631ffa52
Srushtitha16 <msrushtitha@gmail.com>
2026-04-01 14:15:43 +0530

    feature/UC3

--- App/SRC/BookMyStayApp.java
+++ App/SRC/BookMyStayApp.java
@@ -1,5 +1,90 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class RoomInventory {
+
+    private HashMap<String, Integer> inventory;
+
+    public RoomInventory() {
+        inventory = new HashMap<>();
+        inventory.put("Single Room", 5);
+        inventory.put("Double Room", 3);
+        inventory.put("Suite Room", 2);
+    }
+
+    public int getAvailability(String roomType) {
+        return inventory.getOrDefault(roomType, 0);
+    }
+
+    public void updateAvailability(String roomType, int newCount) {
+        inventory.put(roomType, newCount);
+    }
+
+    public void displayInventory() {
+        System.out.println("===== Current Room Inventory =====");
+
+        for (Map.Entry<String, Integer> entry : inventory.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue() + " rooms available");
+        }
+
+        System.out.println("----------------------------------");
+    }
+}
+
 public class BookMyStayApp {
+
+    abstract static class Room {
+        protected String roomType;
+        protected int beds;
+        protected double price;
+
+        public Room(String roomType, int beds, double price) {
+            this.roomType = roomType;
+            this.beds = beds;
+            this.price = price;
+        }
+
+        public void displayRoomDetails() {
+            System.out.println("Room Type: " + roomType);
+            System.out.println("Beds: " + beds);
+            System.out.println("Price per night: ₹" + price);
+        }
+    }
+
+    static class SingleRoom extends Room {
+        public SingleRoom() {
+            super("Single Room", 1, 2000);
+        }
+    }
+
+    static class DoubleRoom extends Room {
+        public DoubleRoom() {
+            super("Double Room", 2, 3500);
+        }
+    }
+
+    static class SuiteRoom extends Room {
+        public SuiteRoom() {
+            super("Suite Room", 3, 6000);
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome to BookMyStay!");
-            }
+
+        System.out.println("===== Book My Stay App =====");
+
+        RoomInventory inventory = new RoomInventory();
+        inventory.displayInventory();
+
+        System.out.println("\nChecking availability for Single Room:");
+        System.out.println("Available: " + inventory.getAvailability("Single Room"));
+
+        System.out.println("\nUpdating Single Room availability...");
+        inventory.updateAvailability("Single Room", 4);
+
+        System.out.println("\nUpdated Inventory:");
+        inventory.displayInventory();
+
+        System.out.println("Application Terminated.");
+    }
 }
